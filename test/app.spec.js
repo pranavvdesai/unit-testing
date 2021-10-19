2c1a12e6c1a4bf1fb8dd253ae72f1285816dd8fd
pranavvdesai <pranavdesai.psd@gmail.com>
2021-10-19 19:45:25 +0530

    added hooks

--- test/app.spec.js
+++ test/app.spec.js
@@ -2,11 +2,35 @@ const Myclass = require('../src/app');
 var myObj = new Myclass();
 const expect = require('chai').expect;
 var sinon = require('sinon');
+const chaiaspromised = require('chai-as-promised');
+// chai.use(chaiaspromised);
 
-describe.skip('Test Suite',()=>{
+describe('Test Suite',()=>{
+    after(function(){
+        console.log('after');
+    })
+    before(function(){
+        console.log('before');
+    })
+    afterEach(function(){
+        console.log('after each test case');
+    })
+    beforeEach(function(){
+        sinon.restore();
+        console.log('before each test case');
+    })
     it('Test the add method',()=>{
         expect(myObj.add(2,3)).to.equal(5);
     })
+    it("spy the add method",()=>{
+        var spy = sinon.spy(myObj,'add');
+        var arg1 = 4, arg2 = 6;
+        myObj.anotherfunc(arg1,arg2);
+        // sinon.assert.calledTwice(spy);
+        expect(spy.calledOnce).to.be.true;
+        expect(spy.calledWith(4,6)).to.be.true;
+    })
+    // hooks before each, restores spy on 'add' method
     it("spy the add method",()=>{
         var spy = sinon.spy(myObj,'add');
         var arg1 = 4, arg2 = 6;
@@ -35,7 +59,7 @@ describe.skip('Test Suite',()=>{
     })
 
 })
-describe('test suite for stub',()=>{
+describe.skip('test suite for stub',()=>{
     it('stub the add method',()=>{
         var stub = sinon.stub(myObj,'add');
         stub.withArgs(4,6).onFirstCall().returns(100000).onSecondCall().returns(200);
@@ -43,4 +67,24 @@ describe('test suite for stub',()=>{
         expect(myObj.anotherfunc(4,6)).to.equal(200);
 
   })
+})
+
+
+describe.skip('test suite for promise',()=>{
+    it('test the promise',function(done){
+        this.timeout(0);
+        myObj.testPromise()
+        .then((res)=>{
+            expect(res).to.equal(10);
+            // expect(false).to.be.true;
+        }).then(done,done)
+    })
+})
+
+
+describe.skip('test suite for cchai-as-promise',()=>{
+    it('test the promise',function(){
+        this.timeout(0);
+        return expect(myObj.testPromise()).to.eventually.equal(10);
+    })
 })
